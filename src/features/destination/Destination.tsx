2200f92f2d9c32f53ed06edce7f8d32673766791
kadar naruto <khadarpaashe123@gmail.com>
2024-12-05 23:43:46 +0300

    added the functionality for the destinations tap

--- src/features/destination/Destination.tsx
+++ src/features/destination/Destination.tsx
@@ -1,8 +1,19 @@
 import Logo from "@/ui/Logo";
 import MobileNav from "@/ui/MobileNav";
 import NavLinks from "@/ui/NavLinks";
+import data from "@/data.json";
+import { useState } from "react";
+
+const { destinations } = data;
 
 function Destination() {
+  const [planet, setPlanet] = useState(0);
+  const [more, setIsMore] = useState(true);
+
+  const currentPlanet = destinations.at(planet);
+  const { name, images, description, distance, travel } = currentPlanet;
+  console.log(images.png);
+
   return (
     <div className="image-destination  h-screen overscroll-none overflow-hidden">
       <div className="grid grid-col-12 grid-rows-[repeat(10,min-content)] overflow-y-auto h-full">
@@ -32,54 +43,66 @@ function Destination() {
             </div>
 
             {/* container for the info */}
+
             <div className="flex lg:flex-row pt-[2.4rem] flex-col justify-between items-center gap-[3.2rem]">
               {/* image */}
               <img
-                src="./image-moon.png"
+                src={images.png}
                 className="h-[15rem] w-[15rem] mb-[3.2rem] md:h-[30rem] md:w-[30rem]"
                 alt=""
               />
 
               {/* titles */}
               <div className="flex flex-col items-center justify-between ">
-                <div className="flex gap-[3.2rem] font-barlowCondensed font-normal justify-center md:w-[70%] text-white text-[1.4rem] md:text-[1.8rem] tracking-widest lg:items-start lg:justify-start">
-                  <div>MOON</div>
-                  <div>MARS</div>
-                  <div>EUROPA</div>
-                  <div>TITAN</div>
+                <div className="flex gap-[3.2rem] font-barlowCondensed font-normal justify-center md:w-[70%] lg:w-[85%] text-white text-[1.4rem] md:text-[1.8rem] lg:translate-x-2 tracking-widest lg:items-start lg:justify-start">
+                  {destinations.map((destination, index) => {
+                    return (
+                      <div
+                        className={`${
+                          planet === index ? "active-nav" : "hover-nav"
+                        } cursor-pointer select-none`}
+                        onClick={() => setPlanet(index)}
+                      >
+                        {destination.name.toUpperCase()}
+                      </div>
+                    );
+                  })}
                 </div>
 
                 {/* topic */}
-                <div className="flex flex-col items-center text-white py-[2.4rem] md:w-[70%] lg:items-start">
+                <div className="flex flex-col items-center text-white py-[2.4rem] md:w-[70%] lg:w-[85%] lg:items-start">
                   <h1 className="font-bellefair text-[5.6rem] md:text-[8rem] font-normal">
-                    MOON
+                    {name.toUpperCase()}
                   </h1>
                   <h2 className="text-center font-barlow text-[1.5rem] md:text-[1.8rem] md:tracking-[180%] tracking-wide lg:text-start">
-                    See our planet as you’ve never seen it before. A perfect
-                    relaxing trip away to help regain perspective and come back
-                    refreshed. While you’re there, take in some history by
-                    visiting the Luna 2 and Apollo 11 landing sites.
+                    {more ? description.slice(0, 100) : description}
+                    <div
+                      className="text-red-300"
+                      onClick={() => setIsMore((show) => !show)}
+                    >
+                      {more ? "more..." : "less..."}
+                    </div>
                   </h2>
                 </div>
 
                 {/* line */}
-                <div className="border-t border-[#979797] h-1 w-full md:w-[70%]"></div>
-                <div className="flex flex-col py-[2.4rem] md:w-[70%] md:flex-row md:justify-between md:items-center">
+                <div className="border-t border-[#979797] h-1 w-full md:w-[70%] lg:w-[85%]"></div>
+                <div className="flex flex-col py-[2.4rem] md:w-[70%] lg:w-[85%] md:flex-row md:justify-between md:items-center">
                   {/* distance */}
                   <div className="flex flex-col items-center lg:items-start text-white py-[2.4rem]">
                     <h1 className="font-barlowCondensed tracking-[0.2rem] text-[1.6rem] font-normal">
                       AVG. DISTANCE
                     </h1>
                     <p className="font-bellefair text-[2.8rem] text-center">
-                      384,400 KM
+                      {distance.toUpperCase()}
                     </p>
                   </div>
                   <div className="flex flex-col items-center lg:items-start text-white py-[2.4rem]">
                     <h1 className="font-barlowCondensed tracking-[0.2rem] text-[1.6rem] font-normal">
-                      Est. TRAVEL TIME
+                      EST. TRAVEL TIME
                     </h1>
                     <p className="font-bellefair text-[2.8rem] text-center">
-                      3 DAYS{" "}
+                      {travel.toUpperCase()}
                     </p>
                   </div>
                 </div>
